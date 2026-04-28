91f4dab42ca2d82069003f01d517c87902dfe8ec
Ivan Leo <ivanleomk@gmail.com>
2026-04-28 08:03:26 -0700

    fix: unused variable in build.zig

--- build.zig
+++ build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const gil_mod = b.addModule("gil", .{
+    _ = b.addModule("gil", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
